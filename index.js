248c260f8f332886d6755a34dabf70e53796313b
Nugrahagateng <nugrahafirizki@gmail.com>
2024-10-24 10:15:53 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//operator aritmatika
+let x = 10;
+let y = 2;
+let z =2* (x + y);
+
+let isAdmIn = true;
+let isLoggedIn = false;
+let age=20;
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+// console.log(++x);
+// console.log(--x);
+// console.log(z);
+
+
+//operator comporison
+// console.log(x== null);
+// console.log(x=== y);
+// console.log(x!==y);
+// console.log(x>y);
+// console.log(x<y);
+
+//operator logika
+console.log(isAdmIn && isLoggedIn);
+console.log(isAdmIn|| isLoggedIn);
+console.log(isAdmIn);
+
+//if else
+if(age >=18){
+    console.log("tua bangka tua mati aja");
+}else{
+    console.log("anjayyyy7uyyy6y6y7");
+}
+
